7c86596d8d32fbfd3bbda15252ce87d67a8cc50e
LabCap <banlazthosow@gmail.com>
2022-09-24 21:30:34 +0300

    added adaptive in header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,2 +1,11 @@
 // Подключение функционала "Чертогов Фрилансера"
 // Подключение списка активных модулей
+
+const burger = document.querySelector(".icon-menu");
+const burgerMenu = document.querySelector(".header-bottom");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("menu-open");
+  burgerMenu.classList.toggle("menu-open");
+  document.body.classList.toggle("_lock");
+});
