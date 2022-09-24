a679a99e430b7bfde010220d1063c6838e3883e8
LabCap <banlazthosow@gmail.com>
2022-09-25 00:02:14 +0300

    added logiс spoilers

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -9,3 +9,14 @@ burger.addEventListener("click", () => {
   burgerMenu.classList.toggle("menu-open");
   document.body.classList.toggle("_lock");
 });
+
+$(document).ready(function () {
+  $(".spoiler-title").click(function (event) {
+    if ($(".footer-top").hasClass("one")) {
+      $(".spoiler-title").not($(this)).removeClass("active");
+      $(".spoiler-list").not($(this).next()).slideUp(300);
+    }
+
+    $(this).toggleClass("active").next().slideToggle(300);
+  });
+});
